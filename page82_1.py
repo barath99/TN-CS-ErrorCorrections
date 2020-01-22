5433d2fd238b5aed9942191801b50c5b710e90b3
barath99 <exploretechsbk@gmail.com>
2020-01-22 13:04:31 +0530

    progress

---
+++ page82_1.py
@@ -0,0 +1,7 @@
+for word in "Jump Statement":
+    if word = = "e":
+        break
+        print (word, end=")
+else:
+    print ("End of the loop")
+print ("\n End of the program") 
---
+++ page84c.py
@@ -0,0 +1,5 @@
+for word in "Jump Statement":
+    if word == "e":
+        continue
+    print (word, end='')
+print ("\nEnd of the program")  
